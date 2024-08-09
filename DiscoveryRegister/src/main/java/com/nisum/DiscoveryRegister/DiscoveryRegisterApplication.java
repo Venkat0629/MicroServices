bb52069af6784e133845e7f3e64cc99673e77c6c
vveera <vveera@nisum.com>
2024-08-09 12:35:52 +0530

    Added Discovery Service

---
+++ DiscoveryRegister/src/main/java/com/nisum/DiscoveryRegister/DiscoveryRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.DiscoveryRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryRegisterApplication.class, args);
+	}
+
+}
---
+++ DiscoveryRegister/src/test/java/com/nisum/DiscoveryRegister/DiscoveryRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.DiscoveryRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
