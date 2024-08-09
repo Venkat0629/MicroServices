9f7dcad13f2392750da919371a3b3d21fc1bbb32
vveera <vveera@nisum.com>
2024-08-09 12:34:26 +0530

    Updated Microsevices Config server, Product, User

--- ConfigServer/src/main/java/com/nisum/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/nisum/ConfigServer/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.nisum.SpringCloudConfigServer;
+package com.nisum.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringCloudConfigServerApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
--- ConfigServer/src/test/java/com/nisum/ConfigServer/ConfigServerApplicationTests.java
+++ ConfigServer/src/test/java/com/nisum/ConfigServer/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nisum.SpringCloudConfigServer;
+package com.nisum.ConfigServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringCloudConfigServerApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
