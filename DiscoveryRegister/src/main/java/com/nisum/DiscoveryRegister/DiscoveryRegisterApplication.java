994a3b1e88dbe84bf0ba926aeb4afc2bbdcbf3f3
vveera <vveera@nisum.com>
2024-08-09 14:56:35 +0530

    Updated Discovery Service

--- DiscoveryRegister/src/main/java/com/nisum/DiscoveryRegister/DiscoveryRegisterApplication.java
+++ DiscoveryRegister/src/main/java/com/nisum/DiscoveryRegister/DiscoveryRegisterApplication.java
@@ -2,8 +2,10 @@ package com.nisum.DiscoveryRegister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryRegisterApplication {
 
 	public static void main(String[] args) {
